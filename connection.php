9e102e4417942c35ba2695d765c2b6c839ae8b13
Luis Duran-Enriquez <89470754+racingturtle12@users.noreply.github.com>
2022-11-10 17:48:18 -0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
      
      $servername = 'localhost';
-     $username = 'icsfa2206';
+     $username = 'ics325fa2206';
      $password = '5859';
-     $database_name = 'icsfa2206';
+     $database_name = 'ics325fa2206';
      $con = mysqli_connect($servername, $username, $password, $database_name);
      ?>
