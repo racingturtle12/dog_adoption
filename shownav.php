7a89f2417ec831c8a54f97391bf0966b5934cdbc
Luis Duran-Enriquez <89470754+racingturtle12@users.noreply.github.com>
2022-11-10 15:00:13 -0600

    Update shownav.php

--- shownav.php
+++ shownav.php
@@ -8,8 +8,8 @@ function shownav() {
         <h1 class="logo">Dog haven<span style="color:#359127;"> Web</span> site</h1>
     </div>
       <a href="index.php">Home</a>
-      <a href="adopt_app.php">Adopt a Dog</a>
-      <a href="adoptable_dogs.php">Adoptable Dogs</a>
+      <a href="adopt_app.php">Adopt a Dog Form </a>
+      <a href="adoptable_dogs.php"> Dogs up for Adoption </a>
       <a href="adoption_agreements.php">Adoption Agreements</a>
       <a href="adoption_fees.php">Adoption Fees</a>
       <a href="loginform.php">Login / Sign up</a>
