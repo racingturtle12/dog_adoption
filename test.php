1dace62339c726e4c45e1ab4a292c18fead82739
Luis Duran-Enriquez <89470754+racingturtle12@users.noreply.github.com>
2022-11-10 16:06:13 -0600

    Update test.php

--- test.php
+++ test.php
@@ -1,21 +1,34 @@
-<html>
-   <head>
-      <title>Connecting MySQL Server</title>
-   </head>
-   <body>
-      <?php
-         $dbhost = 'localhost';
-         $dbuser = 'icsfa2206';
-         $dbpass = 'ukny';
-         $dbname = 'dog_db';
-         $mysqli = new mysqli($dbhost, $dbuser, $dbpass,$dbname);
-         
-         if($mysqli→connect_errno ) {
-            printf("Connect failed: %s<br />", $mysqli→connect_error);
-            exit();
-         }
-         printf('Connected successfully.<br />');
-         $mysqli→close();
-      ?>
-   </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Testing db </title>
+</head>
+<body>
+<?php
+     
+     $dbhost = 'localhost';
+     $dbuser = 'icsfa2206';
+     $dbpass = '5859';
+     $dbname = 'icsfa2206';
+     $con = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+     
+     if($mysqli_connect_errno) {
+        printf("Connect failed: %s<br />", $mysqli_connect_error);
+        exit();
+     }
+     $sql = "SELECT * FROM users";
+     $result = mysqli_query($con,$sql);
+   printf($result);
+     $mysqli→close();
+     
+  ?>
+
+
+
+
+</body>
 </html>
+
