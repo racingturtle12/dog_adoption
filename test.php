19d290d07afd4eec3352da75059e488a2b5bda14
Luis Duran-Enriquez <89470754+racingturtle12@users.noreply.github.com>
2022-11-10 16:50:46 -0600

    Update test.php

--- test.php
+++ test.php
@@ -9,21 +9,29 @@
 <body>
 <?php
      
-     $dbhost = 'localhost';
-     $dbuser = 'icsfa2206';
-     $dbpass = '5859';
-     $dbname = 'icsfa2206';
-     $con = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
-     
-     if($mysqli_connect_errno) {
-        printf("Connect failed: %s<br />", $mysqli_connect_error);
-        exit();
+     $servername = 'localhost';
+     $username = 'icsfa2206';
+     $password = '5859';
+     $database_name = 'icsfa2206';
+     $db = mysqli_connect($servername, $username, $password,$database_name);
+     if(mysqli_connect_errno()){
+echo '<p> Error: Could not get into db. <br/>
+please try again</p>';
+exit;
      }
-     $sql = "SELECT * FROM users";
-     $result = mysqli_query($con,$sql);
-   printf($result);
-     $mysqli→close();
-     
+$querystring = "SELECT * FROM users";
+$stmnt = mysql_query($db, $querystring);
+if (mysqli_num_rows($result) > 0) {
+while($row = mysql_fetch_assoc($stmnt )) {
+   echo "id ". $row['user_id']. "username ". $row['userName']. " passwrof" . $row['password']."email ". $row['user_email']. "<br>";
+}
+}else{
+   echo "0 results";
+}
+
+
+
+
   ?>
 
 
@@ -32,3 +40,7 @@
 </body>
 </html>
 
+<!-- while($row = mysql_fetch_array($stmnt)) {
+   echo $row['column_name']; // Print a single column data
+   echo print_r($row);       // Print the entire row data
+} -->
