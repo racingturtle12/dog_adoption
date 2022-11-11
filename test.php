d67d85f01c9607ad6ea31213af3592a3a8d0e01f
Luis Duran-Enriquez <89470754+racingturtle12@users.noreply.github.com>
2022-11-10 19:34:51 -0600

    Update test.php

--- test.php
+++ test.php
@@ -10,7 +10,7 @@ $resultcheck = mysqli_num_rows($result);
 
 if ($resultcheck > 0) {
 while($row = mysql_fetch_assoc($result)) {
-   echo "id ". $row['user_id']. "username ". $row['userName']. " passwrof" . $row['password']."email ". $row['user_email']. "<br>";
+   echo "id ". $row['user_id']. "username ". $row['userName']. " password" . $row['password']."email ". $row['user_email']. "<br>";
 }
 }
 
