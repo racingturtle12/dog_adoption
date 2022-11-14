7637e5a3dad6d6cd1f350aae9015a5ba173124b8
Luis Duran-Enriquez <89470754+racingturtle12@users.noreply.github.com>
2022-11-14 06:38:59 -0600

    Update registrationform.php

--- registrationform.php
+++ registrationform.php
@@ -1,34 +1,60 @@
+
 <?php
 
 $servername = "localhost";
+
 $username = "ics325fa2202";
+
 $password = "6666";
+
 $dbname = "ics325fa2202";
 
 // Create connection
+
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
 // Check connection
+
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+
+die("Connection failed: " . mysqli_connect_error());
+
 }
-if (isset($_POST['submit'])){}
-        
-        
-        $user = $_POST['userName'];
-        $password = $_POST['password'];
-        $email = $_POST['user_email'];
-        $sql = "INSERT INTO users VALUES ('$user', '$password', '$email')";
-$run = mysqli_query($conn, $sql)
-echo 
-"
-<script>alert('Account creation was successful');</script>
-";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+if (!empty($_POST['userName']) && !empty($_POST['password']) && !empty($_POST['user_email'])){
+
+$user = $_POST['userName'];
+
+$password = $_POST['password'];
+
+$email = $_POST['user_email'];
+
+$sql = "insert into users (userName, password, user_email) values ('$user', '$password', '$email')";
+
+$run = mysqli_query($conn, $sql) or die (mysqli_error());
+
+if($run){
+
+echo "form was succeesful";
+
+} else{
+
+echo "form was no good";
+
+}
+
+}
+
+else{
+
+echo " All fields required";
+
+}
+
+
+}
+
 
 ?>
-
-
-
-
-
-
-
