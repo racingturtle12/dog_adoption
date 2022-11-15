5c494ae7aafaefcc770b9309beabf78e08318f5f
Luis Duran-Enriquez <89470754+racingturtle12@users.noreply.github.com>
2022-11-15 17:00:18 -0600

    changed home by user type

--- admin_nav.php
+++ admin_nav.php
@@ -5,7 +5,7 @@ function show_admin_nav() {
     echo '
     <div class="navbar">
       <h1> Dog Haven Website</h1>
-      <a href="index.php">Home</a>
+      <a href="admin_home.php">Home</a>
       <a href="#">Edit User</a>
       <a href="edit_dog_post.php">Edit Dog Post</a>
       <a href="new_dog.php">Add Dog Post </a>
--- user_nav.php
+++ user_nav.php
@@ -5,7 +5,7 @@ function show_user_nav() {
     echo '
     <div class="navbar">
       <h1> Dog Haven Website</h1>
-      <a href="index.php">Home</a>
+      <a href="user_home.php">Home</a>
       <a href="adopt_app.php">Adopt a Dog Form </a>
       <a href="adoptable_dogs.php"> Dogs up for Adoption </a>
       <a href="adoption_agreements.php">Adoption Agreements</a>
