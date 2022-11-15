13b27bde53760fc1766e121c3a063f674c32a227
Luis Duran-Enriquez <89470754+racingturtle12@users.noreply.github.com>
2022-11-15 13:53:21 -0600

    Update adoptable_dogs.php

--- adoptable_dogs.php
+++ adoptable_dogs.php
@@ -41,8 +41,8 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
   while($row = mysqli_fetch_assoc($result)) {
-    echo  "<br> Dog name: " . $row["dog_name"]. "<br> Dog Breed:  " . $row["dog_breed"]. "<br> Microchipped: " . $row["dog_microchipped"]. "<br> Weight: " . $row["dog_weight"]. "<br> Dog color:  " . $row["dog_color"]. "<br> 
-    Picture: " . '<img src="data:image/png;base64,' .base64_encode( $row["dog_img"]).'"/>';
+    echo  "<br> Dog name: " . $row["dog_name"]. "<br> Dog Breed:  " . $row["dog_breed"]. "<br> Microchipped: " . $row["dog_microchipped"]. "<br> Weight: " . $row["dog_weight"]. "<br> Dog color:  " . $row["dog_color"]. "<br> " 
+    . '<img src="data:image/png;base64,' .base64_encode( $row["dog_img"]).'"/>';
   }
 } else {
   echo "0 results";
