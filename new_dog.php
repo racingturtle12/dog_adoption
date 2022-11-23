dde5c994f7b06517365a6ccabd5f934d135fe83c
Luis Duran-Enriquez <89470754+racingturtle12@users.noreply.github.com>
2022-11-23 07:35:51 -0600

    For admin to add new dog
    admin able to add dog into db which then makes it available on our user page.

--- new_dog.php
+++ new_dog.php
@@ -31,12 +31,12 @@
   <input type="text" id="dogname" name="dog_name" ><br>
   <label for="dog_breed">Dog Breed:</label><br>
   <input type="text" id="dog_breed" name="dog_breed" >
-  <label for="dog_microchipped">Microchipped? (yes or no) :</label><br>
-  <input type="text" id="dog_microchipped" name="dog_microchipped" ><br>
   <label for="dog_weight">Dog Weight:</label><br>
   <input type="text" id="dog_weight" name="dog_weight">
   <label for="dog_color">Dog Color:</label><br>
   <input type="text" id="dog_color" name="dog_color" ><br>
+  <label for="dog_gender">Dog Gender:</label><br>
+  <input type="text" id="dog_gender" name="dog_gender" ><br>
 <label> Only png files </label>
 <br>
 <input type="file" id="dog_img" name="dog_img">
--- new_dog_action.php
+++ new_dog_action.php
@@ -1,46 +1,71 @@
 <?php
 
+$servername = "localhost";
+
+$username = "ics325fa2206";
+
+$password = "5859";
+
+$dbname = "ics325fa2206";
+
+// Create connection
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+
+if (!$conn) {
+
+die("Connection failed: " . mysqli_connect_error());
+
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+$dog_name = $_POST['dog_name'];
+
+$dog_breed = $_POST['dog_breed'];
+$dog_weight = $_POST['dog_weight'];
+$dog_color= $_POST['dog_color'];
+$dog_gender = $_POST['dog_gender'];
+$dog_img = $_POST['dog_img'];
+
+                        // ()Insert names of coloumn in dog table between parenthesis 
+$sql = "INSERT INTO dog ( username, password) VALUES ('$dog_name', '$dog_breed', '$dog_weight', '$dog_color', '$dog_gender', '$dog_img')";
+
+$run = mysqli_query($conn, $sql) or die (mysqli_error());
+
+if($run){
+ 
+echo "New Dog successfully Inserted: "; 
+echo"<br>";
+echo "Dog name:  " . $dog_name;
+echo"<br>";
+echo"<br>";
+echo "Dog Breed:  " . $dog_breed;
+echo"<br>";
+echo"<br>";
+echo "Dog Weight:  " . $dog_weight;
+echo"<br>";
+echo"<br>";
+echo "Dog Color:  " . $dog_color;
+echo"<br>";
+echo"<br>";
+echo "Dog Gender:  " . $dog_gender;
+echo"<br>";
+echo"<br>";
+echo "Picture of Dog:  " . $dog_img;
+echo"<br>";
 
 
 
+} else{
 
+echo "New dog was NOT insteretd ";
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
